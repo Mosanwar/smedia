15e52b66e46ca5b7a6c94c48f5321b7a8b777c47
Mostafa Anwar <mzaid@mum.edu>
2019-12-13 22:37:42 -0600

    update user controller and application.properties

--- src/main/java/edu/mum/ea/socialmedia/controller/UserController.java
+++ src/main/java/edu/mum/ea/socialmedia/controller/UserController.java
@@ -2,12 +2,18 @@ package edu.mum.ea.socialmedia.controller;
 
 import edu.mum.ea.socialmedia.model.User;
 import edu.mum.ea.socialmedia.service.UserService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@Setter
+@Getter
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
