d4666e7ab51adb4ab2828caff3afaa4b9c9678bf
Mostafa Anwar <mzaid@mum.edu>
2019-12-15 20:45:25 -0600

    adding user to the jwt response

--- src/main/java/edu/mum/ea/socialmedia/security/JwtResponse.java
+++ src/main/java/edu/mum/ea/socialmedia/security/JwtResponse.java
@@ -2,17 +2,20 @@ package edu.mum.ea.socialmedia.security;
 
 import java.util.Collection;
 
+import edu.mum.ea.socialmedia.model.User;
 import org.springframework.security.core.GrantedAuthority;
 
 public class JwtResponse {
 	private String token;
 	private String type = "Bearer";
 	private String email;
+	private User user;
 	private Collection<? extends GrantedAuthority> authorities;
 
-	public JwtResponse(String accessToken, String email, Collection<? extends GrantedAuthority> authorities) {
+	public JwtResponse(String accessToken, String email, User user, Collection<? extends GrantedAuthority> authorities) {
 		this.token = accessToken;
 		this.email = email;
+		this.user = user;
 		this.authorities = authorities;
 	}
 
@@ -43,4 +46,12 @@ public class JwtResponse {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
